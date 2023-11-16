e567225ca401eacab14e8f3487b7cf5503ead1e8
Thiago Dantas <148493296+Thiago-Dantas-Araujo@users.noreply.github.com>
2023-11-16 08:19:09 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Thiago Dantas Araújo",
+  title: "Aluno do Curso Superior TSI",
+  email: "dantas.thiago@outlook.com.br",
+  gitHub: "Thiago-Dantas-Araujo",
+  instagram: "ithiagodantas",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
